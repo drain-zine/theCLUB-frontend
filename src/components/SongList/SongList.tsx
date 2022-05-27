a581bfa16133bcefd7694f7e70d950457caa5efa
Tom Stannett <invisibletree@protonmail.ch>
2022-05-27 01:33:26 +0100

    Search Done

--- src/components/SongList/SongList.tsx
+++ src/components/SongList/SongList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { AppReducerState } from '../../reducers/AppReducer';
 import { getSongsFromIds } from '../../selectors/songSelectors';
@@ -8,33 +8,45 @@ import Song from '../Song/Song';
 
 const SongList = ({activePlaylist, songIds}: { activePlaylist: number, songIds: number[]}) => {
     const songs = useSelector((state: AppReducerState) => getSongsFromIds(state, songIds));
-    
-    return (
-        <table className={styles.table}>
-            <thead>
-                <tr className={styles.header}>
-                    <th>TITLE</th>
-                    <th>ARTIST</th>
-                    <th>ALBUM</th>
-                    <th>DURATION</th>
-                    <th></th>
-                    <th></th>
-                </tr>
-            </thead>
+    const [filterText, setFilterText] = useState('');
 
-            <tbody>
-                {songs.map((song: TSong, idx: number) => 
-                    <Song
-                        activePlaylist={activePlaylist}
-                        id={songIds[idx]}
-                        name={song.name}
-                        albumName={song.album.name}
-                        artistName={song.album.artist.name}
-                        duration={'3:20'} />
-                )}
-            </tbody>
-            
-        </table>
+    const filteredSongs = useMemo(() => songs.filter(song => 
+        song.name.toLowerCase().startsWith(filterText.toLowerCase())), 
+    [filterText, songs]);
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.searchbar}>
+                <input value={filterText} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setFilterText(e.target.value)}></input>
+            </div>
+            <table className={styles.table}>
+                <thead>
+                    <tr className={styles.header}>
+                        <th>TITLE</th>
+                        <th>ARTIST</th>
+                        <th>ALBUM</th>
+                        <th>DURATION</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {filteredSongs.map((song: TSong, idx: number) => 
+                        <Song
+                            activePlaylist={activePlaylist}
+                            id={songIds[idx]}
+                            name={song.name}
+                            albumName={song.album.name}
+                            artistName={song.album.artist.name}
+                            duration={'3:20'} />
+                    )}
+                </tbody>
+                
+            </table>
+
+            {songs.length === 0 && <p>No songs have been added to this playlist yet. </p>}
+        </div>
     );
 };
 
